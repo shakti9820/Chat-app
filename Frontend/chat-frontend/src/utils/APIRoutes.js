ed01a49512e0df7c3b054f0e9d4ce9c5ceacbeb7
shakti <shaktisingh979222@gmail.com>
2023-07-26 21:46:28 +0530

    first commit

---
+++ Frontend/chat-frontend/src/utils/APIRoutes.js
@@ -0,0 +1,10 @@
+ 
+export const host = process.env.REACT_APP_BASE_URL;
+console.log(host)
+export const loginRoute = `${host}/api/auth/login`;
+export const registerRoute = `${host}/api/auth/register`;
+export const logoutRoute = `${host}/api/auth/logout`;
+export const allUsersRoute = `${host}/api/auth/allusers`;
+export const sendMessageRoute = `${host}/api/messages/addmsg`;
+export const recieveMessageRoute = `${host}/api/messages/getmsg`;
+
